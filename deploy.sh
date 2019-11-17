e29cd66741b6373b5c77d6b54d83ce6aee98e247
Moustafa Abdelhamid <mou.abdelhamid@gmail.com>
2019-11-17 08:52:15 +0100

    Use Kustomize in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,7 @@ VERSION=$(cat ${APPLICATION_PATH}/.docker-image-version)
 APP_NAME=$(cat ${APPLICATION_PATH}/.docker-image-name)
 PROJECT_ID=$(gcloud config get-value project)
 
-
 docker tag ${APP_NAME}:${VERSION} eu.gcr.io/${PROJECT_ID}/${APP_NAME}:${VERSION}
 docker push eu.gcr.io/${PROJECT_ID}/${APP_NAME}:${VERSION}
-kubectl apply -f ${DEPLOYMENT_PATH}/k8s.yaml
+kustomize build ${DEPLOYMENT_PATH}/prod | kubectl apply -f -
 
-kubectl expose deployment ${APP_NAME} --type=LoadBalancer --port 80 --target-port 3000 #TODO add this to k8s.yaml
